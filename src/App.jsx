f9526ab9266d2149fca5d94da49ecec65ea362e8
billadin <mir.billadin4@gmail.com>
2023-12-21 11:42:51 +0600

    basic routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-function App() {
-  return (
-    <>
-      <h1>Vite + React + Firebase</h1>
-    </>
-  );
-}
+import { createBrowserRouter } from "react-router-dom";
+import { Error, Features, Landing, Login, Pricing, Register } from "./pages";
 
-export default App;
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing/>,
+    errorElement: <Error/>
+  },
+  {
+    path: '/features',
+    element: <Features/>,
+    errorElement: <Error/>
+  },
+  {
+    path: '/pricing',
+    element: <Pricing/>,
+    errorElement: <Error/>
+  },
+  {
+    path: '/login',
+    element: <Login/>,
+    errorElement: <Error/>
+  },
+  {
+    path: '/register',
+    element: <Register/>,
+    errorElement: <Error/>
+  }
+])
+
+export default router;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Error from "./Error";
+import Features from "./Features";
+import Landing from "./Landing";
+import Login from "./Login";
+import Register from "./Register";
+import Pricing from "./Pricing";
+
+export {Error, Features, Landing, Login, Register, Pricing}

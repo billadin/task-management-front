aba23eed1fb95f95c12c0b899f921c128969910e
billadin <mir.billadin4@gmail.com>
2023-12-21 13:01:41 +0600

    navbar setup

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          {/* Navbar menu*/}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <Link to={"/features"}>
+                <li>Features</li>
+              </Link>
+              <Link to={"/pricing"}>
+                <li>Pricing</li>
+              </Link>
+            </ul>
+          </div>
+          {/* Nav Logo */}
+          <Link>
+            <button className="btn btn-ghost text-xl hover:bg-transparent">TASK</button>
+          </Link>
+          {/* Nav menu items */}
+          <div className='hidden lg:inline-flex'>
+            <ul className="menu menu-horizontal px-1 space-x-4">
+              <Link to={"/features"}>
+                <li>Features</li>
+              </Link>
+              <Link to={"/pricing"}>
+                <li>Pricing</li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+
+        {/* Nav items */}
+        <div className="navbar-end space-x-2">
+          <Link to={"/login"}>
+            <button className="btn btn-ghost hover:bg-transparent">Login</button>
+          </Link>
+          <Link to={"/register"}>
+            <button className="btn btn-primary">Get started</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Navbar } from '../Components'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <>
+    <Navbar/>
+    </>
   )
 }
 
